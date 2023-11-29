8dc54661666d242c3d66b8f711bb034a4d9dca28
deepanshu <62979448+deepanshu44@users.noreply.github.com>
2023-11-29 23:28:31 +0530

    now can remove file extensions

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,9 @@ const config = {
             // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
+    resolve : {
+    	extensions: [".js",".jsx"]
+    }
 };
 
 module.exports = () => {
