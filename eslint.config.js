4ef7809d4905c325848f668ecb23ed4cf6396b8b
deepanshu <62979448+deepanshu44@users.noreply.github.com>
2025-11-14 16:08:00 +0530

    update eslint from legacy to flat

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import react from "eslint-plugin-react";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["**/dist"]), {
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended",
+    ),
+
+    plugins: {
+        react,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {
+        semi: "off",
+        quotes: "off",
+        "react/prop-types": [0],
+    },
+}, {
+    files: ["src/**/*.jsx"],
+}]);
